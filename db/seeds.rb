db68d0f551f49b485bd0f638bb0bbf85e1dd4485
Jane Costa <yberdysh@gmail.com>
2017-11-07 12:02:48 -0500

    seeded a user, company, and internship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  Company.create(
+    name: "Unsplash It",
+    industry: "IT",
+    address: "La Gare, Avenue Casgrain #102, Montréal, QC",
+    user_id: 1
+    )
+
+  Internship.create(
+    name: "Dogwalker for CEO",
+    responsibilities: "walking the dog 3 times a day, picking up poop, training the dog",
+    qualifications: "being on time",
+    length: 5,
+    hours: 40,
+    company_id: 1
+    )
