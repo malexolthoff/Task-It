c25232044ac3e2a6dbb3149770fc0757e52f4641
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-07 12:27:39 -0500

    Edited internship controller. May need to be edited further for functionality.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationsController < ApplicationController
+
+  def create
+    Article.create(application_params)
+    redirect_to internship_path
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:message)
+  end
 end
+
