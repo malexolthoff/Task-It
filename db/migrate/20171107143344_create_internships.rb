c0cbed25eb3284efbe3a5eba3adb1f10d83ddd27
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-07 14:03:35 -0500

    Got rid of responsibilities and qualifications. Added description to model.

--- db/migrate/20171107143344_create_internships.rb
+++ db/migrate/20171107143344_create_internships.rb
@@ -2,8 +2,7 @@ class CreateInternships < ActiveRecord::Migration[5.0]
   def change
     create_table :internships do |t|
       t.string :name
-      t.text :responsibilities
-      t.text :qualifications
+      t.text :description
       t.integer :length
       t.integer :hours
       t.references :company, foreign_key: true
