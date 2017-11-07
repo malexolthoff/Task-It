fa45dd3b239d14ea969401330feec9f176c6c615
Jane Costa <yberdysh@gmail.com>
2017-11-07 15:05:10 -0500

    finished companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,4 +3,45 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.find(params[:id])
+    Company.create(company_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    @company.update(company_params)
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :edit
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @user = User.find(@company.user_id)
+    @company.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :industry, :photo, :address, :description)
+  end
+
 end
