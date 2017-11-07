e2044f5bd954c265148caf9a3a970c9162c39e7a
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-07 18:33:11 -0500

    added devise and some views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
-
+  before_action :set_user only
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone, :photo, :description, :education, :experience, :hobbies, :skills, :role)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
+
+
 
 end
