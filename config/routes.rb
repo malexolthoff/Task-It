3c188db74cb73a684b69a54e55cbae051efc998a
Jane Costa <yberdysh@gmail.com>
2017-11-07 11:32:38 -0500

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :companies, only: [:show] do
+    resources :internships
+  end
+
+  resources :internships, only: [:index, :show] do
+    resources :applications, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'articles#index'
 end
