75c2a2f45855a96e2e150daa74d1c9fcdad10a09
Jane Costa <yberdysh@gmail.com>
2017-11-08 20:47:03 -0500

    mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'yberdysh@gmail.com'
   layout 'mailer'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :photo, PhotoUploader
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 end
