f00d562615ff1c1fb25fa6bc98e097e5a752d305
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-10 10:13:28 -0500

    Solved login issues

--- config/application.rb
+++ config/application.rb
@@ -12,4 +12,6 @@ module T
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  # config.action_view.embed_authenticity_token_in_remote_forms = true
 end
