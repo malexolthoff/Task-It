58a617026d799ac4a74a2a6eb5afa40e5dfd547c
Jane Costa <yberdysh@gmail.com>
2017-11-10 14:12:35 -0500

    fixed bug in edit internship

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -35,6 +35,10 @@ class InternshipsController < ApplicationController
 
   def update
     @internship.update(internship_params)
+    if @internship.save
+      redirect_to company_path(@company)
+    else
+      render :edit
   end
 
   def destroy
