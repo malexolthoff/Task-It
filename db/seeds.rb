fded07875c4def3eb07fb8065bd9ef7c2fe9c12e
Alex Olthoff <m.alex.olthoff@gmail.com>
2017-11-10 15:34:26 -0500

    Pushing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Company.destroy_all
 Application.destroy_all
 Internship.destroy_all
+Company.destroy_all
+
 User.destroy_all
 
 
